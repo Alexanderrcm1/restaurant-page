a2f1b701ce042757620dfc4b8a04f4c45379e92e
Alexander Carlsson <a.carlssons98@gmail.com>
2024-07-04 22:08:52 +0200

    imported the menuPage and added event listeners to home and menu buttons

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 import homePage from "./home"
+import menuPage from "./menu"
 
 const content = document.getElementById("content")
+
 const homeBtn = document.getElementById("home-btn")
 const menuBtn = document.getElementById("menu-btn")
-const aboutBtn = document.getElementById("about-btn")
+const contactBtn = document.getElementById("contact-btn")
+
 const createHome = () => {
     const element = document.createElement("div")
     element.setAttribute("class", "homepage")
     element.innerHTML = homePage()
     return element
 }
-content.appendChild(createHome())
+
+const createMenu = () => {
+    const element2 = document.createElement("div")
+    element2.setAttribute("class", "menupage")
+    element2.innerHTML = menuPage()
+    return element2
+}
+content.appendChild(createMenu())
+
+homeBtn.addEventListener("click", () => {
+    content.innerHTML = ""
+    content.appendChild(createHome())
+
+})
+menuBtn.addEventListener("click", () => {
+    content.innerHTML = ""
+    content.appendChild(createMenu())
+})
