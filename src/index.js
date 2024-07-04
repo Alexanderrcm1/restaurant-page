f9ea2ab5ecf591436642ca0e8781cb5e96003ef4
Alexander Carlsson <a.carlssons98@gmail.com>
2024-07-04 21:13:56 +0200

    Got the buttons from the index.html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello")
+import homePage from "./home"
+
+const content = document.getElementById("content")
+const homeBtn = document.getElementById("home-btn")
+const menuBtn = document.getElementById("menu-btn")
+const aboutBtn = document.getElementById("about-btn")
+const createHome = () => {
+    const element = document.createElement("div")
+    element.setAttribute("class", "homepage")
+    element.innerHTML = homePage()
+    return element
+}
+content.appendChild(createHome())
