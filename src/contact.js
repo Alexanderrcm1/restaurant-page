abeae134b0589886f56aa5f2ad331a48de1368a3
Alexander Carlsson <a.carlssons98@gmail.com>
2024-07-06 01:09:07 +0200

    made some changes in the dom manipulation

--- src/contact.js
+++ src/contact.js
@@ -1,26 +1,33 @@
 const contactPage = () => `
-<div class="contact-wrapper>
-    <div class="form-container>
-        <h1 class="contact-title">Contact</h1>
-        <form>
-            <div class="message-container">
-                <label for="message">Your Message: </label>
-                <input type="text" id="message" name="message" required>
-            </div>
-            <div class="name-container>
-                <label for="name">Your Name: </label>
-                <input type="text" id="name" name="name" required>
-            </div>
-            <div class="phone-container">
-                <label for="phone">Your Phone Number: </label>
-                <input type="number" id="phone" name="phone" required>
-            </div>
-            <div class="mail-container">
-                <label for="mail">Your E-mail: </label>
-                <input type="email" id="mail" name="mail">
-            </div>
-        </form>
-    </div>
-</div>
+    <h1 class="contact-title">Contact</h1>
+    <form>
+        <div class="name-container">
+            <label for="name">Your Name: </label>
+            <input type="text" id="name" name="name" required>
+        </div>
+        <div class="phone-container">
+            <label for="phone">Your Phone Number: </label>
+            <input type="number" id="phone" name="phone" required>
+        </div>
+        <div class="mail-container">
+            <label for="mail">Your E-mail: </label>
+            <input type="email" id="mail" name="mail">
+        </div>
+        <div class="message-container">
+            <label for="message">Your Message: </label>
+            <textarea id="message" name="message"></textarea>
+        </div>
+    </form>
+
+    <h4 id="location">Folkungagatan 76,<br> 116 22 Stockholm</h4>
+    <ul>
+        <li>Sunday: 12:00 - 21:00</li>
+        <li>Monday: 10:00 - 20:00</li>
+        <li>Tuesday: 10:00 - 20:00</li>
+        <li>Wednesday: 10:00 - 20:00</li>
+        <li>Thursday: 10:00 - 20:00</li>
+        <li>Friday: 10:00 - 23:00</li>
+        <li>Saturday: 17:00 - 00:00</li>
+    </ul>
 `
 export default contactPage;
