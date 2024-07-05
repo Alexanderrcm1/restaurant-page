052a9296d8cae03049efb61900a42ba717593dcf
Alexander Carlsson <a.carlssons98@gmail.com>
2024-07-06 00:20:42 +0200

    added a url loader rule

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,12 @@ module.exports = {
                 test: /\.css$/i,
                 use: ["style-loader", "css-loader"],
             },
+            {   
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                use: {
+                    loader: "url-loader",
+                },
+            },
         ],
     },
 };
